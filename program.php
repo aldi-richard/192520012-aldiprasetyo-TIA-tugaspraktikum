8d1087bbedb8faaa70a1083a30bd4f284b28d2a7
aldi-richard <91275277+aldi-richard@users.noreply.github.com>
2021-09-23 22:31:53 +0700

    Add files via upload

---
+++ program.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <title>Contoh Biodata</title>
+    </head>
+<body>
+
+<form action="#" style="width: 1300px">
+<table style="width: 600px ; heigh: 600px ;">
+<body style="background-color:blue"> </body>
+
+<tr>
+<td rowspan="10" width="100px">
+<img src="foto.jpg" width="150px" height="200px" border="5">
+</td>
+</tr>
+
+<center>
+<font size="100px"> 
+<b>DATA DIRI</b>
+</font>
+</center>
+
+<tr>
+<td><b>NAMA</b></td>
+<td>:   Aldi Prasetyo</td>
+</tr>
+
+<tr>
+<td><b>NIM</b></td>
+<td>:   192520012</td>
+</tr>
+
+<tr>
+<td><b>ALAMAT</b></td>
+<td>:   Brengkelan Tengah, Purworejo</td>
+</tr>
+
+<tr>
+<td><b>E-MAIL</b></td>
+<td>:   aldipras27@gmail.com</td>
+</tr>
+
+<tr>
+<td><b>MINAT</b></td>
+<td>:   Sistem Operasi Jaringan</td>
+</tr>
+
+</body>
+</html>
